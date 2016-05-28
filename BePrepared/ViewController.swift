e5acb3bd6b66e3b4c62d716e57c786bde2bc93f5
原田　礼朗 <rharada@mba-11z-732.local>
2016-05-28 18:19:17 +0900

    Initial Commit

---
+++ BePrepared/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BePrepared
+//
+//  Created by 原田　礼朗 on 2016/05/28.
+//  Copyright © 2016年 reo harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
